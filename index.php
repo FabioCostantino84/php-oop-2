09c7d02e7d90ceecc77bd618ce9eb8b54e2d1b15
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-22 12:57:42 +0200

    add style with bs5

--- index.php
+++ index.php
@@ -36,7 +36,6 @@ class Product
         $this->image = $image;
         $this->category = $category;
         $this->icon = $icon;
-
     }
 }
 
@@ -52,24 +51,18 @@ class Category
 
 
 $products = [
-    new Product('lovedi', 'giochi', 'Palla Tpr con Punte Fucsia', 4.99, './img/lovedi_palla_fucsia.webp', new Category(['cani']), 'fa-solid fa-dog'),
-    new Product('mast', 'cucce', 'Brandina in alluminio blu', 29.99, './img/mast_brandina.webp', new Category(['cani']),'fa-solid fa-dog'),
-    new Product('monge', 'cibo', 'Monge All Breeds Adult Salmone e Riso', 49.99, './img/monge_salmone_riso.webp', new Category(['cani']),'fa-solid fa-dog'),
-    new Product('natural trainer', 'cibo', 'Natural Trainer Gatto Sterilised Salmone', 2.99, './img/natural_trainer_salmone.webp', new Category(['gatti']), 'fa-solid fa-cat'),
-    new Product('yes', 'giochi', 'Pallina Peluche Rosa', 1.99, './img/pallina_peluche_rosa.webp', new Category(['gatti']), 'fa-solid fa-cat'),
-    new Product('luna e teo', 'cucce', 'Cuccia Ciambella Pelosa Beige', 27.99, './img/ciambella_pelosa_beige.webp', new Category(['gatti']), 'fa-solid fa-cat'),
+    new Product('lovedi', 'giochi', 'Palla Tpr con Punte Fucsia', 4.99, './img/lovedi_palla_fucsia.webp', new Category(['cani']), ''),
+    new Product('mast', 'cucce', 'Brandina in alluminio blu', 29.99, './img/mast_brandina.webp', new Category(['cani']), ''),
+    new Product('monge', 'cibo', 'Monge All Breeds Adult Salmone e Riso', 49.99, './img/monge_salmone_riso.webp', new Category(['cani']), ''),
+    new Product('natural trainer', 'cibo', 'Natural Trainer Gatto Sterilised Salmone', 2.99, './img/natural_trainer_salmone.webp', new Category(['gatti']), ''),
+    new Product('yes', 'giochi', 'Pallina Peluche Rosa', 1.99, './img/pallina_peluche_rosa.webp', new Category(['gatti']), ''),
+    new Product('luna e teo', 'cucce', 'Cuccia Ciambella Pelosa Beige', 27.99, './img/ciambella_pelosa_beige.webp', new Category(['gatti']), ''),
 ];
 
 $animalsType = [
     new Category(['cani']),
     new Category(['gatti']),
 ];
-
-
-
-
-
-
 
 
 ?>
@@ -86,21 +79,24 @@ $animalsType = [
 </head>
 
 <body>
-    <div class="container">
+    <div class="container mt-5">
         <h1>Prodotti</h1>
         <div class="row">
             <?php foreach ($products as $product) : ?>
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="<?php echo $product->image; ?>" class="card-img-top" alt="<?php echo $product->name; ?>">
+                <div class="col-md-4 mb-3">
+                    <div class="card h-100 m-5">
+                        <img src="<?php echo $product->image; ?>" class="card-img-top img-fluid p-5" alt="<?php echo $product->name; ?>">
                         <div class="card-body">
-                            <h5 class="card-title"><?php echo $product->name; ?></h5>
-                            <p class="card-text">Tipo: <?php echo $product->type; ?></p>
-                            <p class="card-text">Descrizione: <?php echo $product->description; ?></p>
-                            <p class="card-text">Prezzo: $<?php echo $product->price; ?></p>
-                            <p class="card-text">Categoria: <?php echo $product->category->animals[0]; ?></p>
-                            <i class="<?php echo $product->icon; ?>"></i>
-                            
+                            <h5 class="card-title text-uppercase"><?php echo $product->name; ?></h5>
+                            <p class="card-text"><strong>Tipo:</strong> <?php echo $product->type; ?></p>
+                            <p class="card-text"><strong>Descrizione:</strong> <?php echo $product->description; ?></p>
+                            <p class="card-text"><strong>Prezzo: €</strong><?php echo $product->price; ?></p>
+                            <p class="card-text"><strong>Categoria:</strong> <?php echo $product->category->animals[0]; ?></p>
+                            <?php if ($product->category->animals[0] === 'cani') : ?>
+                                <i class="fas fa-dog"></i> <!-- Icona cane -->
+                            <?php elseif ($product->category->animals[0] === 'gatti') : ?>
+                                <i class="fas fa-cat"></i> <!-- Icona gatto -->
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
